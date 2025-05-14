59c2650a2ae569b02c378a40bb4e119b5ed617b3
Luk119 <lukasz.kundzicz0190@gmail.com>
2025-05-14 10:19:30 +0200

    najmniejsze kwadraty

--- Lesson11/MetodaNajmniejszychKwadratow.java
+++ Lesson11/MetodaNajmniejszychKwadratow.java
@@ -1,82 +1,85 @@
 package Lesson11;
-//not done
-import java.util.Arrays;
 
+import java.util.Scanner;
+//not done
 public class MetodaNajmniejszychKwadratow {
 
     public static void main(String[] args) {
-        // Przykład z obrazka: sqrt(2x^3 - x + 9), punkty x = {-1, -0.5, 0, 0.5, 1}
-        double[] x = {-1, -0.5, 0, 0.5, 1};
-        double[] y = {
-                Math.sqrt(2*Math.pow(-1,3) - (-1) + 9),
-                Math.sqrt(2*Math.pow(-0.5,3) - (-0.5) + 9),
-                Math.sqrt(2*Math.pow(0,3) - 0 + 9),
-                Math.sqrt(2*Math.pow(0.5,3) - 0.5 + 9),
-                Math.sqrt(2*Math.pow(1,3) - 1 + 9)
-        };
+        Scanner scanner = new Scanner(System.in);
 
-        int n = 2;
-        double punkt = 0.25;
+        System.out.print("Podaj liczbę punktów: ");
+        int m = scanner.nextInt();
+
+        double[] x = new double[m];
+        double[] y = new double[m];
+
+        System.out.println("Podaj punkty x i y:");
+        for (int i = 0; i < m; i++) {
+            System.out.print("x[" + i + "] = ");
+            x[i] = scanner.nextDouble();
+
+            System.out.print("y[" + i + "] = ");
+            y[i] = scanner.nextDouble();
+        }
+
+        System.out.print("Podaj stopień wielomianu aproksymującego: ");
+        int n = scanner.nextInt();
+
+        System.out.print("Podaj punkt x, dla którego obliczyć wartość: ");
+        double punkt = scanner.nextDouble();
 
         double[] wspolczynniki = obliczWspolczynniki(x, y, n);
-        System.out.println("Współczynniki wielomianu: " + Arrays.toString(wspolczynniki));
+
+        System.out.print("Współczynniki wielomianu: ");
+        for(double wsp : wspolczynniki) System.out.print(wsp + " ");
+        System.out.println();
 
         double wartosc = obliczWartosc(wspolczynniki, punkt);
         System.out.println("Wartość w punkcie " + punkt + ": " + wartosc);
+
+        scanner.close();
     }
 
     public static double[] obliczWspolczynniki(double[] x, double[] y, int n) {
         int m = x.length;
-        double[][] S = new double[n+1][n+1];
-        double[] T = new double[n+1];
+        double[][] S = new double[n + 1][n + 1];
+        double[] T = new double[n + 1];
 
-        // Obliczanie macierzy S i wektora T
         for (int k = 0; k <= n; k++) {
             for (int j = 0; j <= n; j++) {
-                S[k][j] = 0;
                 for (int i = 0; i < m; i++) {
-                    S[k][j] += Math.pow(x[i], k+j);
+                    S[k][j] += Math.pow(x[i], k + j);
                 }
             }
 
-            T[k] = 0;
             for (int i = 0; i < m; i++) {
                 T[k] += Math.pow(x[i], k) * y[i];
             }
         }
 
-        // Rozwiązanie układu równań metodą eliminacji Gaussa
         return gaussElimination(S, T);
     }
 
     public static double[] gaussElimination(double[][] A, double[] b) {
         int n = b.length;
 
-        // Eliminacja współczynników
         for (int p = 0; p < n; p++) {
-            // Znajdź wiersz z maksymalnym elementem w kolumnie p
             int max = p;
-            for (int i = p+1; i < n; i++) {
+            for (int i = p + 1; i < n; i++) {
                 if (Math.abs(A[i][p]) > Math.abs(A[max][p])) {
                     max = i;
                 }
             }
 
-            // Zamień wiersze
             double[] temp = A[p];
             A[p] = A[max];
             A[max] = temp;
+
             double t = b[p];
             b[p] = b[max];
             b[max] = t;
 
-            // Jeśli element diagonalny jest bliski zeru, pomiń tę kolumnę
-            if (Math.abs(A[p][p]) <= 1e-10) {
-                continue;
-            }
-
-            // Eliminacja
-            for (int i = p+1; i < n; i++) {
+            for (int i = p + 1; i < n; i++) {
                 double alpha = A[i][p] / A[p][p];
                 b[i] -= alpha * b[p];
                 for (int j = p; j < n; j++) {
@@ -85,22 +88,14 @@ public class MetodaNajmniejszychKwadratow {
             }
         }
 
-        // Podstawienie wsteczne
         double[] x = new double[n];
-        for (int i = n-1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             double sum = 0.0;
-            for (int j = i+1; j < n; j++) {
+            for (int j = i + 1; j < n; j++) {
                 sum += A[i][j] * x[j];
             }
-
-            if (Math.abs(A[i][i]) > 1e-10) {
-                x[i] = (b[i] - sum) / A[i][i];
-            } else {
-                // Jeśli element diagonalny jest zerowy, ustaw współczynnik na 0
-                x[i] = 0;
-            }
+            x[i] = (b[i] - sum) / A[i][i];
         }
-
         return x;
     }
 
