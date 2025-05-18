1e4e6a23cfb57ee61ce4ade12820e587fddf5234
Luk119 <lukasz.kundzicz0190@gmail.com>
2025-05-18 13:18:01 +0200

    najmniejsze kwadraty

--- Lesson11/MetodaNajmniejszychKwadratow.java
+++ Lesson11/MetodaNajmniejszychKwadratow.java
@@ -65,10 +65,48 @@ public class MetodaNajmniejszychKwadratow {
     }
 
     public static double[] gauss(double[][] A, double[] b) {
-        return Aproksymacja.gauss(A, b);
+        int n = b.length;
+
+        for (int p = 0; p < n; p++) {
+            int max = p;
+            for (int i = p + 1; i < n; i++) {
+                if (Math.abs(A[i][p]) > Math.abs(A[max][p])) {
+                    max = i;
+                }
+            }
+
+            double[] temp = A[p];
+            A[p] = A[max];
+            A[max] = temp;
+            double t = b[p];
+            b[p] = b[max];
+            b[max] = t;
+
+            for (int i = p + 1; i < n; i++) {
+                double alpha = A[i][p] / A[p][p];
+                b[i] -= alpha * b[p];
+                for (int j = p; j < n; j++) {
+                    A[i][j] -= alpha * A[p][j];
+                }
+            }
+        }
+
+        double[] x = new double[n];
+        for (int i = n - 1; i >= 0; i--) {
+            double sum = 0.0;
+            for (int j = i + 1; j < n; j++) {
+                sum += A[i][j] * x[j];
+            }
+            x[i] = (b[i] - sum) / A[i][i];
+        }
+        return x;
     }
 
     public static double obliczWartosc(double[] wsp, double x) {
-        return Aproksymacja.obliczW(wsp, x);
+        double result = 0;
+        for (int i = 0; i < wsp.length; i++) {
+            result += wsp[i] * Math.pow(x, i);
+        }
+        return result;
     }
 }
