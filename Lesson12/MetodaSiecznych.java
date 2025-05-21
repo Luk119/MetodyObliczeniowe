90c572d11a455f11f8394f0b923921446cd37a8e
Luk119 <lukasz.kundzicz0190@gmail.com>
2025-05-21 10:12:28 +0200

    error

---
+++ Lesson12/MetodaSiecznych.java
@@ -0,0 +1,64 @@
+package Lesson12;
+import java.util.Scanner;
+
+public class MetodaSiecznych {
+
+    public static double f(double x) {
+        return x * x + 4.1 * x - 6.36;
+    }
+
+    public static double d2f(double x) {
+        return 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Podaj dolną granicę przedziłu(a):");
+        double a = sc.nextDouble();
+
+        System.out.println("Podaj górną granicę przedziłu(b):");
+        double b = sc.nextDouble();
+
+        System.out.println("Podaj wartość błędu(ε):");
+        double epsilon = sc.nextDouble();
+
+        if (f(a) * f(b) >= 0) {
+            System.out.println("Błąd: warunek konieczny nie jest spełniony");
+            return;
+        }
+
+        double x0;
+        boolean fixedA = f(a) * d2f(a) > 0;
+
+        if (fixedA) {
+            x0 = b;
+        } else {
+            x0 = a;
+        }
+
+        double x1;
+        int iterations = 0;
+
+        while (true) {
+            iterations++;
+
+            if (fixedA) {
+                x1 = x0 - f(x0) * (x0 - a) / (f(x0) - f(a));
+            } else {
+                x1 = x0 - f(x0) * (b - x0) / (f(b) - f(x0));
+            }
+
+            if (Math.abs(f(x1)) < epsilon || Math.abs(x1 - x0) < epsilon) {
+                break;
+            }
+
+            x0 = x1;
+        }
+
+        System.out.printf("Przybliżone rozwiązanie: x = %.5f%n", x1);
+        System.out.printf("f(x) = %.5f%n", f(x1));
+        System.out.println("Liczba iteracji: " + iterations);
+    }
+}
+
