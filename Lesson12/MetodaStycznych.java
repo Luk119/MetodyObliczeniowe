8488c679d5c0c1f45d3d66c1518e712ebed125b7
Luk119 <lukasz.kundzicz0190@gmail.com>
2025-05-22 08:42:32 +0200

    Styczne

--- Lesson12/MetodaStycznych.java
+++ Lesson12/MetodaStycznych.java
@@ -18,10 +18,10 @@ public class MetodaStycznych {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Podaj dolną granicę przedziłu(a): ");
+        System.out.print("Podaj dolną granicę przedziału(a): ");
         double a = sc.nextDouble();
 
-        System.out.print("Podaj górną granicę przedziłu(b): ");
+        System.out.print("Podaj górną granicę przedziału(b): ");
         double b = sc.nextDouble();
 
         System.out.print("Podaj wartość błędu(ε): ");
